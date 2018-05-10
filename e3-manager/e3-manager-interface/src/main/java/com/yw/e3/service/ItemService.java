f51425b7d17e1db45dd3836d27540af5d5595a85
e <yanw_mail@sina.com>
2018-05-10 22:27:47 +0800

    Initial commit

---
+++ e3-manager/e3-manager-interface/src/main/java/com/yw/e3/service/ItemService.java
@@ -0,0 +1,7 @@
+package com.yw.e3.service;
+
+import com.yw.e3.pojo.TbItem;
+
+public interface ItemService {
+    TbItem getItemById(Long itemId);
+}
---
+++ e3-manager/e3-manager-web/src/main/java/com/yw/e3/controller/ItemController.java
@@ -0,0 +1,27 @@
+package com.yw.e3.controller;
+
+import com.yw.e3.pojo.TbItem;
+import com.yw.e3.service.ItemService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.xml.ws.RequestWrapper;
+
+/**
+ * 商品管理Controller
+ */
+@Controller
+public class ItemController {
+    @Autowired
+    private ItemService itemService;
+
+    @RequestMapping("/item/{itemId}")
+    @ResponseBody
+    public TbItem getItemById(@PathVariable Long itemId){
+        TbItem tbItem = itemService.getItemById(itemId);
+        return tbItem;
+    }
+}
